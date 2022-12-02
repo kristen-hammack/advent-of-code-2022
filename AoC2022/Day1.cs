c6f2de7d4de3382e5c41223ff13f71bd3d5ef335
Kristen Hammack <kristen.hammack@integrate.com>
2022-12-02 17:45:25 -0600

    Day 1 complete

---
+++ AoC2022/Day1.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var elfCalories = (await File.OpenText("input.txt")
+        .ReadToEndAsync()).Split("\n\n")
+    .Select(s => s.Trim())
+    .Select(s => s.Split("\n")
+        .Select(int.Parse)
+        .Sum()).ToList();
+var sum = 0;
+for (int i = 0; i < 3; i++)
+{
+    var max = elfCalories.Max();
+    sum += max;
+    elfCalories.Remove(max);
+}
+Console.WriteLine(sum);
