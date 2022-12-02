98b4d1d9296b7bbef10239938d23501aeed57f4e
Kristen Hammack <kristen.hammack@integrate.com>
2022-12-02 17:35:19 -0600

    Editorconfig bc oy

--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine((await File.OpenText("input.txt").ReadToEndAsync()).Split("\n\n").Select(s=>s.Trim()).Select(s=>s.Split("\n").Select(int.Parse).Sum()).Max());
+Console.WriteLine((await File.OpenText("input.txt")
+        .ReadToEndAsync()).Split("\n\n")
+    .Select(s => s.Trim())
+    .Select(s => s.Split("\n")
+        .Select(int.Parse)
+        .Sum())
+    .Max());
