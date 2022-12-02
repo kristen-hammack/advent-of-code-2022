d8b6e5e9bb44f3e2a38b253b7e610229a614f235
Kristen Hammack <kristen.hammack@integrate.com>
2022-12-02 17:34:00 -0600

    First star

--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine((await File.OpenText("input.txt").ReadToEndAsync()).Split("\n\n").Select(s=>s.Trim()).Select(s=>s.Split("\n").Select(int.Parse).Sum()).Max());
