35881f7d516bbce900e5f9d10c98bab2a3dc28a0
Kristen Hammack <kristen.hammack@integrate.com>
2022-12-02 18:15:55 -0600

    Setup rock paper scissors operators

---
+++ AoC2022/Day2.cs
@@ -0,0 +1,28 @@
+﻿namespace aoc;
+
+public class Day2
+{
+    public async Task Do()
+    {
+        var input =(await File.OpenText("Day1input.txt").ReadToEndAsync()).Split("\n");
+        //x=1, y=2, z=3
+        //x>c, y>a, z>b
+        
+    }
+    
+}
+
+public class rps
+{
+    public char C { get; set; }
+
+    public static implicit operator rps(char c) => new() { C = c };
+    
+    public static bool operator >(rps r, rps p)
+    {
+        return r != p && (r.C == 'x' ? p.C == 'c' : r.C == 'y' ? p.C == 'a' : p.C == 'b');
+    }
+    public static bool operator <(rps r, rps p) => !(r > p) && r != p;
+    public static bool operator ==(rps r, rps p) => r.C == p.C;
+    public static bool operator !=(rps r, rps p) => r.C != p.C;
+}
